4df63c1bbf1e510b3ff23d2f390942fa8299eceb
Samathy Barratt <samathy@sbarratt.co.uk>
2020-12-31 13:01:32 +0000

    Provide method to set default values for attibutes

--- source/dattrs.d
+++ source/dattrs.d
@@ -34,3 +34,27 @@ mixin template define(string[] names, ARGS...)
         mixin(t," "~names[i]~";");
     }
 }
+
+/** Add default values for attributes. Optionally define a no-argument this. */
+mixin template define_defaults(string[] names, string[] defaults, bool overload, ARGS...)
+{
+    static if (overload)
+    {
+        mixin("this(){}");
+    }
+
+    mixin("this("~this_args!(names, ARGS)~"){"~this_body!(names, ARGS)~"}");
+
+    static foreach(int i, t; ARGS)
+    {
+        //If the default value is a string, we need to add quote marks;
+        static if ( is(t == string))
+        {
+            mixin(t," "~names[i]~" = \""~defaults[i]~"\";");
+        }
+        else
+        {
+            mixin(t," "~names[i]~" = "~defaults[i]~";");
+        }
+    }
+}
